2bca6a29734a082d33fb390a2d11309c931707ea
ArjunKSanthosh <arjunk80043@gmail.com>
2024-07-17 09:09:03 +0530

    array

---
+++ JS/array.js
@@ -0,0 +1,16 @@
+a=[1,2,3,"hai","hello","good",true,false,'hello']
+console.log(a);
+console.log(a[5]);
+console.log(a.length);
+console.log(a.indexOf("hello"));
+a.push("mern")
+console.log(a);
+a.pop();
+console.log(a);
+a.shift();
+console.log(a);
+a.splice(a.indexOf(3),1)
+console.log(a);
+console.log(a.toString());
+console.log(a.reverse());
+console.log(a.join(" "));
---
+++ JS/string.js
@@ -0,0 +1,18 @@
+str="hello-world"
+// console.log(str);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase().toLowerCase().length);
+// console.log(str.trim());
+// console.log(str.trimLeft());
+// console.log(str.trimRight());
+// console.log(str.substring(0,3));
+// console.log(str.replace("hello","hai"));
+// console.log(str.startsWith("h"));
+// console.log(str.endsWith("ld"));
+// arr=str.split("-")
+// console.log(arr);
+// st=arr.join("-");
+// console.log(st);
+
